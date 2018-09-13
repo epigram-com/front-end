df18d13dd6a67efeaffacb44af530adc8aa1288a
eliast9 <38985331+eliast9@users.noreply.github.com>
2018-09-13 21:46:02 +0300

    Update append_answer_to_post.js

--- append_answer_to_post.js
+++ append_answer_to_post.js
@@ -1,5 +1,9 @@
-function append_answer_to_post(post, answer) {
-    var epigram_node = get_epigram_node(post);
-    var text_node = document.createTextNode(" | " + answer);
-    epigram_node.appendChild(text_node);
+function get_callback_for_http(post) {
+    return function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var epigram_node = get_epigram_node(post);
+            var text_node = document.createTextNode(" | " + this.responseText);
+            epigram_node.appendChild(text_node);
+        }
+    };
 }
