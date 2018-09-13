25242716597fb5a36c97029ec3f3bcd56ef3bb36
eliast9 <38985331+eliast9@users.noreply.github.com>
2018-09-13 21:46:37 +0300

    Update dom_utils.js

--- dom_utils.js
+++ dom_utils.js
@@ -7,13 +7,14 @@ function get_title_node(post_node) {
 }
 
 function get_epigram_node(post_node) {
-    var epigram_nodes = $(post_node).children(".epigram");
+    var epigram_nodes = $(post_node).find(".epigram");
+    var epigram_node;
     if (!epigram_nodes.length) {
         epigram_node = document.createElement("a");
         epigram_node.className = "epigram";
         post_node.append(epigram_node);
     } else {
-        var epigram_node = epigram_nodes[0];
+        epigram_node = epigram_nodes[0];
     }
     return epigram_node;
 }
