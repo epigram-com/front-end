30513ce31ac4abf4718c2aa402b4ccb918a97455
tamer <tamer.mour@gmail.com>
2018-09-13 16:13:38 +0300

    add append answers script.

---
+++ append_answer_to_post.js
@@ -0,0 +1,4 @@
+function append_answer_to_post(post, answer) {
+    var $(post).select(".mbs _5wj- _6m6 _2cnj _5s6c");
+
+}
--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 $(document).on('DOMNodeInserted', function(elt) {
     if(elt.className == "_3ekx _29_4"){
         var z = elt.childNodes;
