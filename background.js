ec413dda5e676459de2eee4c7979a3adf5a1edd2
tamer <tamer.mour@gmail.com>
2018-09-13 15:38:13 +0300

    Fix content/background scripts.

--- background.js
+++ background.js
@@ -2,19 +2,5 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
     if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
         chrome.tabs.executeScript(null, { "file" : "content.js" },function () {});
-        $(document).on('DOMNodeInserted', function(elt) {
-            if(elt.className == "_3ekx _29_4"){
-            var z = elt.childNodes;
-            if(z[0]) {
-                var m = z[0].getElementsByClassName("_59tj _2iau");
-                if (m.length) {
-                    var child = elt.querySelector("a");
-                    var y = child.getAttribute("href");
-                    console.log(y.toString());
-                }
-            }
-            }
-        });
     }
-
 });
--- content.js
+++ content.js
@@ -1,14 +1,13 @@
-
-var elements = document.getElementsByClassName("_3ekx _29_4");
-for (elt of elements) {
-
-    var z = elt.childNodes;
-    if(z[0]) {
-        var m = z[0].getElementsByClassName("_59tj _2iau");
-        if (m.length) {
-            var child = elt.querySelector("a");
-            var y = child.getAttribute("href");
-            console.log(y.toString());
+$(document).on('DOMNodeInserted', function(elt) {
+    if(elt.className == "_3ekx _29_4"){
+        var z = elt.childNodes;
+        if(z[0]) {
+            var m = z[0].getElementsByClassName("_59tj _2iau");
+            if (m.length) {
+                var child = elt.querySelector("a");
+                var y = child.getAttribute("href");
+                console.log(y.toString());
+            }
         }
     }
-}
+});
