830d90d5edaef44828670c860f4b883b3c9e4bbc
youseft42 <ytannous20@gmail.com>
2018-09-13 17:51:11 +0300

    manifest, background and content

--- content.js
+++ content.js
@@ -1,17 +1,28 @@
 
-$(document).on('DOMNodeInserted', function(elt) {
-    console.log(elt.className);
-    if(elt.className == "_3ekx _29_4"){
+$(document).on('DOMNodeInserted', function(element) {
+    var node = element.target;
+    var post_nodes = $(node).find("div._29_4");
+    for(elt of post_nodes){
         var z = elt.childNodes;
-        if(z[0]) {
+        if (z[0]) {
             var m = z[0].getElementsByClassName("_59tj _2iau");
             if (m.length) {
+                var mbs = z[0].getElementsByClassName("mbs");
+                console.log(mbs);
                 var child = elt.querySelector("a");
                 var y = child.getAttribute("href");
-                console.log(y.toString());
+                var link = y.toString();
+                chrome.storage.sync.set({ link : link ,post : mbs}, function() {
+                    console.log('Value is set to ' + mbs);
+                });
+                chrome.storage.sync.get([ 'post' ], function(result) {
+                    console.log('Value currently is ' + result.link);
+                });
+
             }
         }
     }
+
 });
 // var elements = document.getElementsByClassName("_3ekx _29_4");
 // for(elt of elements){
