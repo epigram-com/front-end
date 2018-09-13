956ba946f2729174119b214ab965acc0f28db2c3
youseft42 <ytannous20@gmail.com>
2018-09-13 18:09:42 +0300

    all scripts included

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
     chrome.tabs.executeScript(null, { "file" : "jquery-3.3.1.min.js" },function () {});
+    chrome.tabs.executeScript(null, { "file" : "dom_utils.js" },function () {});
+    chrome.tabs.executeScript(null, { "file" : "append_answer_to_post.js" },function () {});
     if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
 
         chrome.tabs.executeScript(null, { "file" : "content.js" },function () {});
