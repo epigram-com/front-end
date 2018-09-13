3620035da1802a0efd8931e283861c608e0c9d16
tamer <tamer.mour@gmail.com>
2018-09-13 17:52:09 +0300

    add some shit.

--- append_answer_to_post.js
+++ append_answer_to_post.js
@@ -1,4 +1,5 @@
 function append_answer_to_post(post, answer) {
-    var $(post).select(".mbs _5wj- _6m6 _2cnj _5s6c");
-
+    var epigram_node = post.get_epigram_node(post);
+    var text_node = document.createTextNode(" | " + answer);
+    epigram_node.appendChild(text_node);
 }
