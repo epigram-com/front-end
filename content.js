e35b5f11e8c964c7aa19e6ab934c9063ac6451de
tamer <tamer.mour@gmail.com>
2018-09-13 15:52:37 +0300

    include jQuery.

--- content.js
+++ content.js
@@ -1,9 +1,3 @@
-var script = document.createElement('script');
-script.src = 'jquery-3.3.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
 $(document).on('DOMNodeInserted', function(elt) {
     if(elt.className == "_3ekx _29_4"){
         var z = elt.childNodes;
