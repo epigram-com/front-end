d3976f5b4f6c4b6969acb6e727a9a781933ee13e
youseft42 <ytannous20@gmail.com>
2018-09-13 18:02:56 +0300

    manifest, background and content

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-
+const SERVER = "";
 $(document).on('DOMNodeInserted', function(element) {
     var node = element.target;
     var post_nodes = $(node).find("div._29_4");
@@ -7,23 +7,27 @@ $(document).on('DOMNodeInserted', function(element) {
         if (z[0]) {
             var m = z[0].getElementsByClassName("_59tj _2iau");
             if (m.length) {
-                var mbs = z[0].getElementsByClassName("mbs");
-                console.log(mbs);
-                var child = elt.querySelector("a");
-                var y = child.getAttribute("href");
-                var link = y.toString();
-                chrome.storage.sync.set({ link : link ,post : mbs}, function() {
-                    console.log('Value is set to ' + mbs);
-                });
-                chrome.storage.sync.get([ 'post' ], function(result) {
-                    console.log('Value currently is ' + result.link);
-                });
+                var post = z[0].getElementsByClassName("mbs");
+                console.log(post);
+                var link = get_link(post);
+                var http_query =  new XMLHttpRequest();
+                http_query.onreadystatechange = function(){
+                    append_answer_to_post(post, this.responseText);
+                };
+                http_query.open("GET",SERVER + "?link=" + link,true);
+                http_query.send();
 
             }
         }
     }
 
 });
+// chrome.storage.sync.set({ link : link ,post : mbs}, function() {
+//     console.log('Value is set to ' + mbs);
+// });
+// chrome.storage.sync.get([ 'post' ], function(result) {
+//     console.log('Value currently is ' + result.link);
+// });
 // var elements = document.getElementsByClassName("_3ekx _29_4");
 // for(elt of elements){
 //     var z = elt.childNodes;
