ff0f78649dc16925b2f2bf634df92d122334f48c
tamer <tamer.mour@gmail.com>
2018-09-13 17:29:03 +0300

    add dom utils file.

---
+++ dom_utils.js
@@ -0,0 +1,17 @@
+$.fn.exists = function () {
+    return this.length !== 0;
+}
+
+function get_title_node(post_node) {
+    return $(post_node).children("a");
+}
+
+function get_epigram_node(post_node) {
+    var epigram_node = $(post).children(".epigram");
+    if (!epigram_node.exists()) {
+        epigram_node = document.createElement("a");
+        epigram_node.className = "epigram";
+        post_node.append(epigram_node);
+    }
+    return epigram_node;
+}
